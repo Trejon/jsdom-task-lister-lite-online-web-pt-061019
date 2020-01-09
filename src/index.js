05d94103465e9102b42e2a7b0f1844c13109d5e7
github username <github email address>
2020-01-08 22:03:22 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  start();
 });
+
+function start() {
+  const form = document.getElementById("create-task-form");
+  form.addEventListener('submit', processRequest);
+}
+
+function processRequest(event) {
+  event.preventDefault();
+  const task = document.getElementById("new-task-description");
+  addToList(task.value);
+  task.value = "";
+}
+
+function addToList(task) {
+  const list = document.getElementById('tasks');
+  const item = document.createElement('li');
+  item.innerHTML = task;
+  list.appendChild(item);
+  item.appendChild(spawnDeleteButton());
+  document.getElementById("tasks").appendChild(item);
+}
+
+function deleteItem() {
+  document.getElementById("tasks").removeChild(this.parentElement)
+}
+
+function spawnDeleteButton() {
+  let deleteButton = document.createElement('button');
+  deleteButton.innerHTML = "delete";
+  deleteButton.addEventListener("click", deleteItem);
+  return deleteButton;
+}
